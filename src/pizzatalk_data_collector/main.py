5afe1218e90957961ccaca4ada384bd4659f20b3
Quoc Toan Tran <quoctoan09072002@gmail.com>
2024-02-08 11:03:35 +0700

    feat(labeller): add aggregator to combine same day responses, fixed AB#27

--- src/pizzatalk_data_collector/main.py
+++ src/pizzatalk_data_collector/main.py
@@ -11,6 +11,11 @@ from labeller.aggregator import aggregate_today_response
 # deals = crawler.crawl("deal")
 # products = crawler.crawl("product")
 
+today = datetime.now()
+if today.hour == 0 and today.minute <= 10:
+    yesterday = datetime.now() - timedelta(days=1)
+    aggregate_today_response(yesterday.strftime("%Y%m%d"))
+
 chatgpt = ChatGPTGenerator(
     chrome_path='"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"'
 )
